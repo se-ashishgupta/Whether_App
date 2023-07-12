e2c8b970da83a079a980c31066e9ce2f53f3aff9
Ashish Gupta <gashish4950@gmail.com>
2023-07-12 19:54:15 +0530

    initial commit

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-export const server = "http://localhost:4000";
+export const server = "https://whetherappserver.onrender.com";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
