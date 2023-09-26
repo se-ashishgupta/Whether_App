4f6211d86a271a96995e24b09cbb4e8ee3f5460a
achintya-dev-repo <achintyasolutions.webdev@gmail.com>
2023-09-26 17:47:52 +0530

    update

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-export const server = "http://localhost:4000";
+export const server = process.env.REACT_APP_SERVER;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
